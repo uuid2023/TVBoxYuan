69fdcbd6b9d648161e28e8d18e0f1223691b7355
Zhong <39251951+uuid2023@users.noreply.github.com>
2023-02-05 05:55:16 +0800

    Update jabletv.js

--- JS/jabletv.js
+++ JS/jabletv.js
@@ -6,8 +6,8 @@ var rule = {
         'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
-    class_name:'最新上市',//静态分类名称拼接
-    class_url:'new-release',//静态分类标识拼接
+    class_name:'中文字幕&凌辱強暴',//静态分类名称拼接
+    class_url:'categories/chinese-subtitle/&categories/rape/',//静态分类标识拼接
     //class_parse:'ul.animenu__nav&&li;a&&Text;a&&href',
     limit:5,
     play_parse:true,
